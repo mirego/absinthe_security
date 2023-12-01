626478f2fca2910213150d98e710dc2071aa482a
Mathieu Legault <mathieu.legault.5@gmail.com>
2023-12-01 10:56:21 -0500

    remove boilerplate acronym

--- .credo.exs
+++ .credo.exs
@@ -75,7 +75,7 @@
           # Naming
           {CredoNaming.Check.Warning.AvoidSpecificTermsInModuleNames, terms: ["Manager", "Fetcher", "Builder", "Persister", "Serializer", ~r/^Helpers?$/i, ~r/^Utils?$/i]},
           {CredoNaming.Check.Consistency.ModuleFilename,
-           excluded_paths: ["config", "mix.exs", "priv", "test/support"], acronyms: [{"ElixirBoilerplateGraphQL", "elixir_boilerplate_graphql"}, {"GraphQL", "graphql"}]}
+           excluded_paths: ["config", "mix.exs", "priv", "test/support"], acronyms: []}
         ],
         disabled: [
           {Credo.Check.Consistency.MultiAliasImportRequireUse, []},
